a9eef27b87f9e0f88fd637362d04e7e815cc3685
Michael Pu <Michael.Pu2@student.tdsb.on.ca>
2017-05-26 08:58:12 -0400

    Inital Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+//when document is ready
+$(document).ready(pageReady);
+
+//page ready
+function pageReady() {
+    //button press
+    $("button").click(buttonClicked);
+    $(window).scroll(pageScroll);
+}
+
+//button clicked
+function buttonClicked() {
+    var buttonID = this.id;
+    console.log(buttonID + "button clicked");
+
+    switch(buttonID) {
+        case "busPredictorPage":
+            window.open("../BusPredictionsWebpage", "_self");
+    }
+}
+
+//page scroll
+function pageScroll() {
+    if ($(window).scrollTop() > 0) {
+        $('#navBar').addClass("nav-bar-fixed");
+    }
+    else {
+        $('#navBar').removeClass("nav-bar-fixed");
+    }
+}
