7366a65d16fd487231d7b76dfd740605dd497c9f
Michael Pu <michael.pu123@gmail.com>
2017-05-26 22:27:30 -0400

    added all folders

---
+++ pages/photography/photography.js
@@ -0,0 +1,7 @@
+//when document is ready
+$(document).ready(pageReady);
+
+//page ready
+function pageReady() {
+    $("#photography").addClass("w3-green");
+}
---
+++ pages/video/video.js
@@ -0,0 +1,7 @@
+//when document is ready
+$(document).ready(pageReady);
+
+//page ready
+function pageReady() {
+    $("#video").addClass("w3-green");
+}
