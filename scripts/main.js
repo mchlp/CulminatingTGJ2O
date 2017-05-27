4e6f4b63377f974c76b59c19b2f465f9f688808b
Michael Pu <michael.pu123@gmail.com>
2017-05-26 22:07:01 -0400

    improved page navigation

--- scripts/main.js
+++ scripts/main.js
@@ -3,25 +3,7 @@ $(document).ready(pageReady);
 
 //page ready
 function pageReady() {
-    //button press
-    $("button").click(buttonClicked);
     $(window).scroll(pageScroll);
-}
-
-//button clicked
-function buttonClicked() {
-    var buttonID = this.id;
-    console.log(buttonID + "button clicked");
-
-    switch (buttonID) {
-        case "home":
-            window.open("/CulminatingTGJ2O/", "_self");
-            break;
-        case "photoshop":
-            window.open("/CulminatingTGJ2O/pages/photoshop", "_self");
-            break;
-
-    }
 }
 
 //page scroll
