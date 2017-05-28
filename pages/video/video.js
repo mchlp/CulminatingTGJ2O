d28b51495327c2fda51f9291d1ac3b2523a1caf4
Michael Pu <michael.pu123@gmail.com>
2017-05-28 10:55:46 -0400

    added card to videos

--- pages/video/video.js
+++ pages/video/video.js
@@ -3,5 +3,13 @@ $(document).ready(pageReady);
 
 //page ready
 function pageReady() {
+    $("button").click(buttonPress);
     $("#video").addClass("w3-green");
+}
+
+//button pressed
+function buttonPress() {
+    var buttonID = this.id;
+    console.log(buttonID);
+    //switch (buttonID)
 }
