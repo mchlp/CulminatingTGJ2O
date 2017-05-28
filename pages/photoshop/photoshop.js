94efda0b039e3cb1e64bcce330f255571cc5c25c
Michael Pu <michael.pu123@gmail.com>
2017-05-28 16:04:17 -0400

    added click listener for images

--- pages/photoshop/photoshop.js
+++ pages/photoshop/photoshop.js
@@ -4,4 +4,10 @@ $(document).ready(pageReady);
 //page ready
 function pageReady() {
     $("#photoshop").addClass("w3-green");
+    $("._image").click(buttonPress);
+}
+
+function buttonPress() {
+    buttonID = this.id;
+    console.log(buttonID);
 }
