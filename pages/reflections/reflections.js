351ba703b5f3205ad00e1b5cb2b8118fc8b6377a
Michael Pu <michael.pu123@gmail.com>
2017-06-11 19:11:28 -0400

    added reflection tempalte

---
+++ pages/reflections/reflections.js
@@ -0,0 +1,29 @@
+var slideIndex = 1;
+
+//when document is ready
+$(document).ready(pageReady);
+
+//page ready
+function pageReady() {
+    $("#reflections").addClass("w3-green");
+    showDivs(slideIndex);
+}
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
