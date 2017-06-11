19a5eff361e0761d9250da78e9369313c9811023
Michael Pu <michael.pu123@gmail.com>
2017-06-11 14:49:22 -0400

    added statement of intent

---
+++ pages/planning/research/research.js
@@ -0,0 +1,8 @@
+//when document is ready
+$(document).ready(pageReady);
+
+//page ready
+function pageReady() {
+    $("#planning").addClass("w3-green");
+    $("#research").addClass("w3-green");
+}
